ab14fdc7c62add416af54c328f5a4ffc1f3e866d
Michael Brown <michael.entr@gmail.com>
2018-04-30 07:49:47 -0400

    added image uploader and display image

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ end
   gem 'bootstrap', '~> 4.0.0'
   gem 'simple_form'
   gem 'devise'
+  gem 'carrierwave', '~> 1.0'
   
   source 'https://rails-assets.org' do
     gem 'rails-assets-tether', '>= 1.3.3'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426234909) do
+ActiveRecord::Schema.define(version: 20180430113716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180426234909) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
     t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
   end
 
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180426234909) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
